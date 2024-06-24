ab93d110e377b31fe821ba9b5f137160aa8b87de
Asrith Rajeevan <asrithrajeevan@Asriths-MacBook-Air.local>
2024-06-24 12:33:31 +0530

    new changes

--- ceba_builders/ceba_builders/settings.py
+++ ceba_builders/ceba_builders/settings.py
@@ -22,6 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 env = environ.Env(DEBUG=(bool, False))
 env_file = os.path.join(BASE_DIR, '.env')
+print(env_file)
 env.read_env(env_file)
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 
